414a11aebe3532df710cf1b32ef1ecac123482bd
lamidimorufat <lamidimorufat0@gmail.com>
2024-03-06 20:24:57 +0100

    modify the readme and js file

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const menu = document.querySelector(".menu");
+const openNav = document.querySelector("#open-nav");
+const closeNav = document.querySelector("#close-nav");
 const dropdowns = document.querySelectorAll(".sub-list");
 const arrow = document.querySelectorAll(".sub-menu");
 const dropdownMenus = document.querySelectorAll(".dropdown"); 
 
+openNav.addEventListener("click", () => {
+    menu.style.display = "flex";
+    openNav.style.display = "none";
+    closeNav.style.display = "inline-block";
+})
+closeNav.addEventListener("click", () =>{
+    menu.style.display = "none";
+    openNav.style.display = "inline-block";
+    closeNav.style.display = "none";
+})
 
 dropdowns.forEach((dropdown, i) => {
     dropdown.addEventListener("click", () => {
@@ -19,18 +32,7 @@ dropdowns.forEach((dropdown, i) => {
     });
 });
 
-const menu = document.querySelector(".menu");
-const openNav = document.querySelector("#open-nav");
-const closeNav = document.querySelector("#close-nav");
 
-openNav.addEventListener("click", () => {
-    menu.style.display = "flex";
-    openNav.style.display = "none";
-    closeNav.style.display = "inline-block";
-})
-closeNav.addEventListener("click", () =>{
-    menu.style.display = "none";
-    openNav.style.display = "inline-block";
-    closeNav.style.display = "none";
-})
+
+
 
